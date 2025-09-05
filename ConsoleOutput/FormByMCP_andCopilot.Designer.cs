c81a582efd3041af5c11f8d6232856ac4175e880
AVM <andre.moreira@petrosoft.pt>
2025-09-05 15:28:56 +0100

    Added new form FormByMCP_andCopilot for issue #2

---
+++ ConsoleOutput/FormByMCP_andCopilot.Designer.cs
@@ -0,0 +1,45 @@
+namespace ConsoleOutput
+{
+    partial class FormByMCP_andCopilot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormByMCP_andCopilot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormByMCP_andCopilot";
+            this.Text = "Form Created by MCP and Copilot";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
